f2a1c0930ad7df29c311554c5cd3c8ee110877f7
RenoFar <renaudfaraci@gmail.com>
2021-03-11 12:32:38 +0100

    introducing auth

--- home/views.py
+++ home/views.py
@@ -1,9 +1,36 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 def index(request):
-    return render(request, 'home/home.html')
+
+	if request.method == 'post':
+		request.post.get('name')
+		request.post.get('password')
+
+		user = authenticate(request, username=name, password=password)
+
+		if user is not None:
+			Login(request, user)
+			return redirect('flux')	
+
+	return render(request, 'home/home.html')
 
 
 def registration(request):
-    return render(request, 'registration/inscription.html')
+	form = UserCreationForm()
+
+	if request.method == 'post':
+		form = UserCreationForm(request.post)
+		if form.is_valid():
+			form.save()
+			user_name = form.cleaned_data.get('username')
+			messages.success(request, 'Votre compte "' + user_name 
+				+ '" est bien enregistré')
+
+			return redirect('home')
+
+	context = {'form': form}
+
+	return render(request, 'registration/inscription.html', context)
